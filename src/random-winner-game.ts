7e707aa868e197848940bee6e0561212dbacd8b2
VishalPachpor <Vishalpatil080502@gmail.com>
2023-08-22 19:33:04 +0530

    function handleGameEnded

--- src/random-winner-game.ts
+++ src/random-winner-game.ts
@@ -7,18 +7,19 @@ import {
 } from "../generated/RandomWinnerGame/RandomWinnerGame";
 import { Game } from "../generated/schema";
 
-export function handleGameEnded(event: GameEndedEvent): void {
-  let entity = new GameEnded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
+export function handleGameEnded(event: GameEnded): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Game.load(event.params.gameId.toString());
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    return;
+  }
+  // Entity fields can be set based on event parameters
   entity.winner = event.params.winner
   entity.requestId = event.params.requestId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
+  // Entities can be written to the store with `.save()`
   entity.save()
 }
 
